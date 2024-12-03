f29782c39d256ebd5046f2119e2b3d6e282c7c92
nadeemshare <58810566+nadeemshare@users.noreply.github.com>
2024-12-03 12:11:34 +0530

    Update optionalChainingOperator.js

--- optionalChainingOperator.js
+++ optionalChainingOperator.js
@@ -2,7 +2,12 @@ let person = {
     name: "John",
     age: 20
 };
-
+function showTotal() {
+  var num1 = 10;
+  var num2 = 20;
+  var total = num1 + num2;
+  alert('Total is : ' + total);
+}
 console.info(person.foo?.bar); // undefined
 console.info(person?.name); // John
 
